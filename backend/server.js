ab1f1adcfb249683daca627cb870ca24a308572b
Padma Ganesh <ganeshpadma730@gmail.com>
2026-03-19 12:58:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -210,9 +210,10 @@ app.delete("/delete-lead/:id", async (req, res) => {
 
 /* ================= SERVER ================= */
 
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 module.exports = app;
